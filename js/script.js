66af3a2df1c95c58a57e867e5e60ce0b5366da15
puhrome <phirom.yim@fullsail.edu>
2014-05-22 16:23:38 -0400

    Conditionals
    Using if else statements to explain a conditional in WebStorm

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Conditionals Created by puhrome on 5/22/14.
+ *
+if(condition){
+    actions
+
+}
+sunny=true;//if condition is true
+
+if(sunny){//set to perform
+    goToTheBeach();
+}
+
+sunny=false;// if condition is false
+
+if(sunny){
+    goToTheBeach();//ignore
+}else{
+    goToTheMovies();//perform this condition instead
+}
